daa9cf28db3af2ec0002286e93efea720f83753b
Lars Beuster <l.beuster@cartelsol.com>
2020-09-24 10:28:41 +0200

    Tracked down the issue to one resources-config with 2 different patterns

--- src/main/java/de/lbe/Main.java
+++ src/main/java/de/lbe/Main.java
@@ -6,6 +6,7 @@ import java.util.Enumeration;
 public class Main {
 
     public static void main(final String[] args) throws Exception {
+        System.out.println("GraalVM-Version: " + System.getProperty("java.vm.version"));
         Enumeration<URL> resources = Main.class.getClassLoader().getResources("graalvm-test.txt");
         while (resources.hasMoreElements()) {
             Object url = resources.nextElement();
